58f4edd521e1f5842948ed85ecfca6d908ab30f3
Dean Iverson <diverson@vtti.vt.edu>
2021-11-30 22:39:17 -0700

    Solutions for day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,56 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = self::parse_config(&args);
+
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let depths: Vec<i32> = contents
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|d_str| d_str.parse::<i32>().unwrap())
+        .collect();
+
+    count_depths_1(&depths);
+    count_depths_2(&depths);
+}
+
+fn parse_config(args: &[String]) -> &str {
+    &args[1]
+}
+
+fn count_depths_1(depths: &Vec<i32>) {
+    let mut incs = 0;
+
+    for window in depths[..].windows(2) {
+        if window[1] > window[0] {
+            incs += 1;
+        }
+    }
+
+    println!("window size 2 increases: {}", incs);
+}
+
+fn count_depths_2(depths: &Vec<i32>) {
+    let mut incs = 0;
+    let mut last = 0;
+
+    for window in depths[..].windows(3) {
+        let sum: i32 = window.iter().sum();
+        if last > 0 {
+            if sum > last {
+                incs += 1;
+            }
+        }
+
+        last = sum;
+    }
+
+    println!("window size 3 increases: {}", incs);
 }
