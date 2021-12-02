c14966fa653806331efdcf88d262c1408c993443
Dean Iverson <diverson@vtti.vt.edu>
2021-12-01 20:53:00 -0700

    Added package for Day 2

---
+++ day02/src/main.rs
@@ -0,0 +1,51 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = parse_config(&args);
+
+    println!("Reading from: {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Could not read data file");
+
+    let depths: Vec<i32> = contents
+        .split("\n")
+        .map(|d_str| d_str.parse().expect("File contained a non-integer value!"))
+        .collect();
+
+    count_depths_1(&depths);
+    count_depths_2(&depths);
+}
+
+fn parse_config(args: &[String]) -> &str {
+    &args[1]
+}
+
+fn count_depths_1(depths: &[i32]) {
+    let mut incs = 0;
+
+    for window in depths.windows(2) {
+        if window[1] > window[0] {
+            incs += 1;
+        }
+    }
+
+    println!("Part 1 increases: {}", incs);
+}
+
+fn count_depths_2(depths: &[i32]) {
+    let mut incs = 0;
+    let mut last = 0;
+
+    for window in depths.windows(3) {
+        let sum: i32 = window.iter().sum();
+        if last > 0 && sum > last {
+            incs += 1;
+        }
+
+        last = sum;
+    }
+
+    println!("Part 2 increases: {}", incs);
+}
