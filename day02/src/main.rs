844f208a198ac80062c6fc10c3d2b25fe5d49372
Dean Iverson <diverson@vtti.vt.edu>
2021-12-01 22:31:56 -0700

    Solution for day 2.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -9,43 +9,56 @@ fn main() {
 
     let contents = fs::read_to_string(filename).expect("Could not read data file");
 
-    let depths: Vec<i32> = contents
-        .split("\n")
-        .map(|d_str| d_str.parse().expect("File contained a non-integer value!"))
-        .collect();
+    let navs: Vec<&str> = contents.split("\n").collect();
 
-    count_depths_1(&depths);
-    count_depths_2(&depths);
+    pilot_sub_1(&navs);
+    pilot_sub_2(&navs);
 }
 
 fn parse_config(args: &[String]) -> &str {
     &args[1]
 }
 
-fn count_depths_1(depths: &[i32]) {
-    let mut incs = 0;
+fn pilot_sub_1(navs: &Vec<&str>) {
+    let mut pos = 0u32;
+    let mut depth = 0u32;
 
-    for window in depths.windows(2) {
-        if window[1] > window[0] {
-            incs += 1;
+    for nav in navs {
+        let parts: Vec<&str> = nav.split(" ").collect();
+        let cmd = parts[0];
+        let dist: u32 = parts[1].parse().expect("Expected number here!");
+
+        match cmd {
+            "forward" => pos += dist,
+            "down" => depth += dist,
+            "up" => depth -= dist,
+            _ => (),
         }
     }
 
-    println!("Part 1 increases: {}", incs);
+    println!("Part 1 answer: {}", pos * depth);
 }
 
-fn count_depths_2(depths: &[i32]) {
-    let mut incs = 0;
-    let mut last = 0;
+fn pilot_sub_2(navs: &Vec<&str>) {
+    let mut aim = 0u32;
+    let mut pos = 0u32;
+    let mut depth = 0u32;
 
-    for window in depths.windows(3) {
-        let sum: i32 = window.iter().sum();
-        if last > 0 && sum > last {
-            incs += 1;
+    for nav in navs {
+        let parts: Vec<&str> = nav.split(" ").collect();
+        let cmd = parts[0];
+        let dist: u32 = parts[1].parse().expect("Expected number here!");
+
+        match cmd {
+            "forward" => {
+                pos += dist;
+                depth += aim * dist;
+            }
+            "down" => aim += dist,
+            "up" => aim -= dist,
+            _ => (),
         }
-
-        last = sum;
     }
 
-    println!("Part 2 increases: {}", incs);
+    println!("Part 2 answer: {}", pos * depth);
 }
