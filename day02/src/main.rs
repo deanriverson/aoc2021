2f26a277b18adc11a62ae58507004a2b479eebcc
Dean Iverson <diverson@vtti.vt.edu>
2021-12-02 21:36:35 -0700

    Playing with a more Alex-like functional solution

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,5 +1,4 @@
-use std::env;
-use std::fs;
+use std::{env, fs, slice::Iter, str};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,56 +8,73 @@ fn main() {
 
     let contents = fs::read_to_string(filename).expect("Could not read data file");
 
-    let navs: Vec<&str> = contents.split("\n").collect();
+    let navs: Vec<(&str, u32)> = contents.split("\n").map(line_to_str_u32).collect();
 
-    pilot_sub_1(&navs);
-    pilot_sub_2(&navs);
+    calc_solution_1(navs.iter());
+    calc_solution_2(navs.iter());
+}
+
+// fn calc_solution_1(navs: &Vec<(&str, u32)>) {
+// let mut pos = 0u32;
+// let mut depth = 0u32;
+
+// for nav in navs {
+//     match nav {
+//         ("forward", dist) => pos += dist,
+//         ("down", dist) => depth += dist,
+//         ("up", dist) => depth -= dist,
+//         _ => (),
+//     };
+// }
+//
+// println!("Part 1 answer: {}", pos * depth);
+// }
+
+fn calc_solution_1(navs: Iter<(&str, u32)>) {
+    let res = navs.fold((0, 0), |(pos, depth), nav| {
+        return match nav {
+            ("forward", dist) => (pos + dist, depth),
+            ("down", dist) => (pos, depth + dist),
+            ("up", dist) => (pos, depth - dist),
+            _ => (pos, depth),
+        };
+    });
+
+    println!("Part 1 answer: {}", res.0 * res.1);
+}
+
+fn calc_solution_2(navs: Iter<(&str, u32)>) {
+    let mut aim = 0u32;
+    let mut pos = 0u32;
+    let mut depth = 0u32;
+
+    for nav in navs {
+        match nav {
+            ("forward", dist) => {
+                pos += dist;
+                depth += aim * dist;
+            }
+            ("down", dist) => aim += dist,
+            ("up", dist) => aim -= dist,
+            _ => (),
+        }
+    }
+
+    println!("Part 2 answer: {}", pos * depth);
 }
 
 fn parse_config(args: &[String]) -> &str {
     &args[1]
 }
 
-fn pilot_sub_1(navs: &Vec<&str>) {
-    let mut pos = 0u32;
-    let mut depth = 0u32;
-
-    for nav in navs {
-        let parts: Vec<&str> = nav.split(" ").collect();
-        let cmd = parts[0];
-        let dist: u32 = parts[1].parse().expect("Expected number here!");
-
-        match cmd {
-            "forward" => pos += dist,
-            "down" => depth += dist,
-            "up" => depth -= dist,
-            _ => (),
-        }
-    }
-
-    println!("Part 1 answer: {}", pos * depth);
-}
-
-fn pilot_sub_2(navs: &Vec<&str>) {
-    let mut aim = 0u32;
-    let mut pos = 0u32;
-    let mut depth = 0u32;
-
-    for nav in navs {
-        let parts: Vec<&str> = nav.split(" ").collect();
-        let cmd = parts[0];
-        let dist: u32 = parts[1].parse().expect("Expected number here!");
-
-        match cmd {
-            "forward" => {
-                pos += dist;
-                depth += aim * dist;
-            }
-            "down" => aim += dist,
-            "up" => aim -= dist,
-            _ => (),
-        }
-    }
-
-    println!("Part 2 answer: {}", pos * depth);
+fn line_to_str_u32(line: &str) -> (&str, u32) {
+    let mut parts = line.split(" ");
+    (
+        parts.next().unwrap(),
+        parts
+            .next()
+            .unwrap()
+            .parse::<u32>()
+            .expect("Expected number here!"),
+    )
 }
