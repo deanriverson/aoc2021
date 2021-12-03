4fcdfb5ef13fe212fea9f6b1918e7aff7652e6b1
Dean Iverson <diverson@vtti.vt.edu>
2021-12-03 09:06:57 -0700

    Solutions for day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, slice::Iter, str};
+use std::{env, fs, str};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,74 +7,97 @@ fn main() {
     println!("Reading from: {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Could not read data file");
+    let lines: Vec<&str> = contents.split("\n").collect();
 
-    let navs: Vec<(&str, u32)> = contents.split("\n").map(line_to_str_u32).collect();
-
-    calc_solution_1(navs.iter());
-    calc_solution_2(navs.iter());
+    calc_solution_1(&lines);
+    calc_solution_2(&lines);
 }
 
-// fn calc_solution_1(navs: &Vec<(&str, u32)>) {
-// let mut pos = 0u32;
-// let mut depth = 0u32;
+fn calc_solution_1(lines: &Vec<&str>) {
+    let mut gamma = 0u32;
+    let mut epsilon = 0u32;
 
-// for nav in navs {
-//     match nav {
-//         ("forward", dist) => pos += dist,
-//         ("down", dist) => depth += dist,
-//         ("up", dist) => depth -= dist,
-//         _ => (),
-//     };
-// }
-//
-// println!("Part 1 answer: {}", pos * depth);
-// }
+    let line_len = lines[0].len();
+    let line_count = lines.len();
 
-fn calc_solution_1(navs: Iter<(&str, u32)>) {
-    let res = navs.fold((0, 0), |(pos, depth), nav| {
-        return match nav {
-            ("forward", dist) => (pos + dist, depth),
-            ("down", dist) => (pos, depth + dist),
-            ("up", dist) => (pos, depth - dist),
-            _ => (pos, depth),
-        };
-    });
+    for i in 0..line_len {
+        let mut one_count = 0;
 
-    println!("Part 1 answer: {}", res.0 * res.1);
-}
-
-fn calc_solution_2(navs: Iter<(&str, u32)>) {
-    let mut aim = 0u32;
-    let mut pos = 0u32;
-    let mut depth = 0u32;
-
-    for nav in navs {
-        match nav {
-            ("forward", dist) => {
-                pos += dist;
-                depth += aim * dist;
+        for line in lines {
+            if line.as_bytes()[i] == ('1' as u8) {
+                one_count += 1;
             }
-            ("down", dist) => aim += dist,
-            ("up", dist) => aim -= dist,
-            _ => (),
+        }
+
+        gamma = gamma << 1;
+        epsilon = epsilon << 1;
+
+        if one_count > line_count / 2 {
+            gamma = gamma | 1;
+        } else {
+            epsilon = epsilon | 1;
         }
     }
 
-    println!("Part 2 answer: {}", pos * depth);
+    println!("Part 1 answer: {}", gamma * epsilon);
+}
+
+fn calc_solution_2(lines: &Vec<&str>) {
+    let oxy = find_life_support(lines, true);
+    let co2 = find_life_support(lines, false);
+    println!("Part 2 answer: {}", oxy * co2);
+}
+
+fn find_life_support(lines: &Vec<&str>, is_oxy: bool) -> u32 {
+    let line_len = lines[0].len();
+    let mut lines = lines.clone();
+
+    for i in 0..line_len {
+        let init: (Vec<&str>, Vec<&str>) = (Vec::new(), Vec::new());
+
+        let res = lines.iter().fold(init, |mut acc, line| {
+            if line.as_bytes()[i] == ('1' as u8) {
+                acc.1.push(line);
+            } else {
+                acc.0.push(line);
+            }
+            acc
+        });
+
+        lines = if is_oxy {
+            select_gt(&res.0, &res.1)
+        } else {
+            select_lte(&res.0, &res.1)
+        };
+
+        if lines.len() == 1 {
+            return parse_bin_str(lines[0]);
+        }
+    }
+
+    return 0;
+}
+
+fn select_gt<'a, 'b>(fst: &'a Vec<&'b str>, snd: &'a Vec<&'b str>) -> Vec<&'b str> {
+    if fst.len() > snd.len() {
+        fst.clone()
+    } else {
+        snd.clone()
+    }
+}
+
+fn select_lte<'a, 'b>(fst: &'a Vec<&'b str>, snd: &'a Vec<&'b str>) -> Vec<&'b str> {
+    if fst.len() <= snd.len() {
+        fst.clone()
+    } else {
+        snd.clone()
+    }
 }
 
 fn parse_config(args: &[String]) -> &str {
     &args[1]
 }
 
-fn line_to_str_u32(line: &str) -> (&str, u32) {
-    let mut parts = line.split(" ");
-    (
-        parts.next().unwrap(),
-        parts
-            .next()
-            .unwrap()
-            .parse::<u32>()
-            .expect("Expected number here!"),
-    )
+fn parse_bin_str(s: &str) -> u32 {
+    isize::from_str_radix(s, 2).unwrap() as u32
 }
