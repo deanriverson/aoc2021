9b4d64276580a4a923dd20d9501218164f29eb27
Dean Iverson <diverson@vtti.vt.edu>
2021-12-06 15:43:02 -0700

    Change from HashMap to array-based solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::{env, fs, str};
 
 fn main() {
@@ -14,12 +13,12 @@ fn main() {
 }
 
 fn calc_solution_1(contents: &str) {
-    let fish = parse_numbers(contents);
+    let fish = parse_numbers_to_array(contents);
     println!("Part 1 answer: {}", age_fish_for_days(fish, 80));
 }
 
 fn calc_solution_2(contents: &str) {
-    let fish = parse_numbers(contents);
+    let fish = parse_numbers_to_array(contents);
     println!("Part 2 answer: {}", age_fish_for_days(fish, 256));
 }
 
@@ -27,13 +26,34 @@ fn parse_config(args: &[String]) -> &str {
     &args[1]
 }
 
-fn age_fish_for_days(mut fish: HashMap<i64, i64>, d: i32) -> i64 {
+fn parse_numbers_to_array(contents: &str) -> [i64; 9] {
+    contents
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .fold([0i64; 9], |mut a, n: usize| {
+            a[n] += 1;
+            a
+        })
+}
+
+fn age_fish_for_days(mut fish: [i64; 9], d: i32) -> i64 {
     for _ in 0..d {
         fish = age_fish(fish);
     }
-
-    fish.into_values().sum::<i64>()
+    fish.iter().sum::<i64>()
 }
+
+fn age_fish(fish: [i64; 9]) -> [i64; 9] {
+    let new_day = [0i64, 0, 0, 0, 0, 0, fish[0], 0, fish[0]];
+
+    (1..9).fold(new_day, |mut arr, i| {
+        arr[i - 1] += fish[i];
+        arr
+    })
+}
+
+/* My original HashMap impl.
+use std::collections::HashMap;
 
 fn parse_numbers(contents: &str) -> HashMap<i64, i64> {
     contents
@@ -43,6 +63,14 @@ fn parse_numbers(contents: &str) -> HashMap<i64, i64> {
             map.entry(n).and_modify(add(1)).or_insert(1);
             map
         })
+}
+
+fn age_fish_for_days(mut fish: HashMap<i64, i64>, d: i32) -> i64 {
+    for _ in 0..d {
+        fish = age_fish(fish);
+    }
+
+    fish.iter().sum::<i64>()
 }
 
 fn age_fish(mut fish: HashMap<i64, i64>) -> HashMap<i64, i64> {
@@ -61,3 +89,4 @@ fn age_fish(mut fish: HashMap<i64, i64>) -> HashMap<i64, i64> {
 fn add(x: i64) -> Box<dyn Fn(&mut i64)> {
     Box::new(move |i: &mut i64| *i += x)
 }
+*/
